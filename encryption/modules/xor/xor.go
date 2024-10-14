c1d0c193ecbdddf9ccafe64d41e94e30dd060214
mixati <mr.mishatit@gmail.com>
2024-10-14 23:06:10 +0300

    xor

--- encryption/modules/xor/xor.go
+++ encryption/modules/xor/xor.go
@@ -1,8 +1,7 @@
 package xor
 
-func XOREncryption(num, key int) int {
+func Encryption(num, key int) int {
 	ans := 0
-	// level := 1
 	for level := 1; num != 0; level *= 10 {
 		ans = ans + ((num%10)^key)*level
 		num /= 10
